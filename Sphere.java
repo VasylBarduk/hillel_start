41844261760a54bf5f917ca83dca0b83f9714230
VasylBarduk <115087747+VasylBarduk@users.noreply.github.com>
2022-10-07 18:41:52 +0300

    Add files via upload

---
+++ Sphere.java
@@ -0,0 +1,15 @@
+package Lesson3;
+
+public class Sphere {
+    public static void main(String[] args ) {
+        int rOne = 1;
+        double rTwo = 0.5;
+        double rThre = 0.2;
+        double density = 0.7;
+        double volume = 4/3 * 3.14 * ((rOne+rTwo+rThre)*(rOne+rTwo+rThre)*(rOne+rTwo+rThre));
+        double weightBall = density*volume;
+        System.out.println("Weight Ball = " +weightBall);
+        }
+
+}
+
