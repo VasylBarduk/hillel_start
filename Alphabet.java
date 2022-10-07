4e3c40966b7f6fc5090d2e76fad155c2127abad4
VasylBarduk <115087747+VasylBarduk@users.noreply.github.com>
2022-10-07 20:17:50 +0300

    Add files via upload

---
+++ Alphabet.java
@@ -0,0 +1,29 @@
+package Alphabet;
+
+public class Alphabet {
+    public static void main(String[] args) {
+        int rangeRandom = rangeRandom(45, 190);
+        isAlphabet((char) rangeRandom);
+    }
+
+    static int rangeRandom(int start, int end) {
+        double random = Math.random();
+        return (char) (random * (end - start + 1)) + start;
+    }
+
+    static void isAlphabet(char randomChar) {
+        System.out.println("Значение " +  randomChar);
+
+       if (randomChar == ('a'-'z') || randomChar == ('A'-'Z'))
+        {
+            System.out.println("is a Alphabet" );
+        }
+       else if (randomChar != ('a'-'z') || randomChar !=('A'-'Z')) {
+           System.out.println("is a Symbol");
+
+       }
+
+
+        }
+    }
+
