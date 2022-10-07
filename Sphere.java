2083e8d5298fc7843e560cc562d8e79ac0f93132
VasylBarduk <115087747+VasylBarduk@users.noreply.github.com>
2022-10-07 18:44:34 +0300

    Update Sphere.java

--- Sphere.java
+++ Sphere.java
@@ -4,8 +4,8 @@ public class Sphere {
     public static void main(String[] args ) {
         int rOne = 1;
         double rTwo = 0.8;
-        double rThre = 0.2;
-        double density = 0.1;
+        double rThre = 0.9;
+        double density = 0.4;
         double volume = 4/3 * 3.14 * ((rOne+rTwo+rThre)*(rOne+rTwo+rThre)*(rOne+rTwo+rThre));
         double weightBall = density*volume;
         System.out.println("Weight = " +weightBall);
