493bb82cbb926fa2090375a8323461585e8dce52
VasylBarduk <115087747+VasylBarduk@users.noreply.github.com>
2022-10-07 19:18:35 +0300

    Update SanSiro.java

--- SanSiro.java
+++ SanSiro.java
@@ -1,9 +1,9 @@
 public class SanSiro {
     public static void main (String [] args) {
-        double latitude = 45.47827100;
-        double longitude = 9.12396200;
-        System.out.println( "Latitude= " +latitude);
-        System.out.println( "Longitude= " +longitude);
+        double latitude = 55.47827100;
+        double longitude = 49.12396200;
+        System.out.println( "широта= " +latitude);
+        System.out.println( "долгота= " +longitude);
     }
 }
 
