7e9d6f2837bdd78e4f0a43f31ede3f6eb3d5f963
VasylBarduk <115087747+VasylBarduk@users.noreply.github.com>
2022-10-07 19:17:38 +0300

    Add files via upload

---
+++ SanSiro.java
@@ -0,0 +1,10 @@
+public class SanSiro {
+    public static void main (String [] args) {
+        double latitude = 45.47827100;
+        double longitude = 9.12396200;
+        System.out.println( "Latitude= " +latitude);
+        System.out.println( "Longitude= " +longitude);
+    }
+}
+
+
